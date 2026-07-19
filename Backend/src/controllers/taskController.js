e7a8767de9fafbe48d8be497cf840053c28f4875
Rohit <rhajare851@gmail.com>
2026-07-19 18:00:20 +0530

    taskcontroller is coded

---
+++ Backend/src/controllers/taskController.js
@@ -0,0 +1,26 @@
+const Task = require("../models/Task");
+
+const createTask = async (req,res) =>{
+    try{
+        const {title,description} = req.body;
+
+        const task = await Task.create({
+            title,
+            description,
+            user: req.user._id,
+        });
+
+        res.status(201).json({   //!201 Created → Used when you've created a new resource.
+            message : "Task Created Successfully",
+            task,
+        });
+
+
+    }catch(error){
+       console.log(error);
+
+       res.status(500).json({
+            message : "Server Error"
+       });
+    }
+}
