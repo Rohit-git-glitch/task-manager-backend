1663ed319d15f0489c137742e7ed2580438e33ed
Rohit <rhajare851@gmail.com>
2026-07-24 23:39:37 +0530

    ok

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,4 +1,7 @@
-
+const {
+    successResponse,
+    errorResponse
+} = require("../utils/response");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -11,9 +14,16 @@ const registerUser = async(req, res,next) => {   //! This is actually good it pr
     const existingUser = await User.findOne({email});
 
     if(existingUser){
-        return res.status(400).json({
-            message : "User already exists",
-        });
+        // return res.status(400).json({
+        //     message : "User already exists",
+        // });
+        return errorResponse(
+            res,
+            400,
+            "User already exists",
+            "USER_ALREADY_EXISTS"
+        );
+
     }
     
     const hashedPassword = await bcrypt.hash(password,10);  //todo here 10 is the salting factor good balance secure and slower
@@ -25,14 +35,24 @@ const registerUser = async(req, res,next) => {   //! This is actually good it pr
             password : hashedPassword
         });
 
-        res.status(201).json({
-            message: "User registered Successfully",
-            user: {
+        // res.status(201).json({
+        //     message: "User registered Successfully",
+        //     user: {
+        //         id: user._id,
+        //         name: user.name,
+        //         email: user.email
+        //     }
+        // });
+        return successResponse(
+            res,
+            201,
+            "User registered Successfully",
+            {
                 id: user._id,
                 name: user.name,
                 email: user.email
             }
-        });
+        );
     } catch(error){
         next(error);
     }
@@ -50,17 +70,29 @@ const loginUser = async(req , res , next) => {
         const user = await User.findOne({email});
 
         if(!user){
-            return res.status(400).json({
-                message : "Invalid Credentials"
-            });
+            // return res.status(400).json({
+            //     message : "Invalid Credentials"
+            // });
+            return errorResponse(
+                res,
+                400,
+                "Invalid Credentials",
+                "INVALID_CREDENTIALS"
+            );
         }
 
         const isMatch = await bcrypt.compare(password , user.password);
 
         if(!isMatch){
-            return res.status(400).json({
-                message : "Invalid Credentials"
-            });
+            // return res.status(400).json({
+            //     message : "Invalid Credentials"
+            // });
+            return errorResponse(
+                res,
+                400,
+                "Invalid Credentials",
+                "INVALID_CREDENTIALS"
+            );
         }
 
         const token = jwt.sign(                      //todo jwt.sign() creates a signed JWT token by taking a payload (like the user's id), signing it with a secret key, and optionally adding an expiration time.
@@ -72,20 +104,39 @@ const loginUser = async(req , res , next) => {
              }
             );
 
-        res.status(200).json({
-            message : "Login Successful",
-            token : token
-        })
+        // res.status(200).json({
+        //     message : "Login Successful",
+        //     token : token
+        // })
+        return successResponse(
+            res,
+            200,
+            "Login Successful",
+            {
+                token 
+            }
+        );
 
     }catch(error){
-      next();
+      next(error);
     }
 };
 
 
 const getProfile = (req , res, next) => {
     try{
-         res.status(200).json(req.user);
+        //  res.status(200).json(req.user);
+        return successResponse(
+            res,
+            200,
+            "Profile fetched successfully",
+            {
+                id: req.user._id,
+                name: req.user.name,
+                email: req.user.email
+             }
+
+        );
     }catch(error){
         next(error);
     }
