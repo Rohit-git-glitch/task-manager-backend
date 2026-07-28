16642ba7a0e1de22ad96dec1eb0c50b9a39990e2
Rohit <rhajare851@gmail.com>
2026-07-28 21:50:39 +0530

    added login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,74 @@
+import { useState } from "react";
+import API from "../services/api";
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // console.log(formData);
+
+    try {
+    const response = await API.post("/users/login", formData);
+
+    console.log(response.data);
+
+    alert("Login successful");
+
+  } catch (error) {
+    console.log(error.response?.data || error.message);
+
+    alert("Login failed");
+  }
+  };
+
   return (
-    <h1 className="text-3xl font-bold text-center mt-10">
-      Login Page
-    </h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-lg shadow-md w-96"
+      >
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Login
+        </h1>
+
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          className="w-full border p-2 mb-4 rounded"
+        />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          className="w-full border p-2 mb-4 rounded"
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+        >
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
 
